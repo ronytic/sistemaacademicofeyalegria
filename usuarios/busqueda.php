8b1f192fc6645a0281731d5c968f8670ecf423ae
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-02-20 21:01:40 -0400

    Correción

--- usuarios/busqueda.php
+++ usuarios/busqueda.php
@@ -7,8 +7,22 @@ if (!empty($_POST)) {
 	${$narchivo}=new $narchivo;
 	extract($_POST);
 	$nivel=$nivel?" nivel='$nivel' ":" nivel LIKE '%' ";
-	$datos=${$narchivo}->mostrarTodo("nombre LIKE '%$nombre%' and paterno LIKE '%$paterno%' and materno LIKE '%$materno%' and nivel!=1 and $nivel");
-	$titulo=array("usuario"=>"Usuario","nombre"=>"Nombre","paterno"=>"Paterno","materno"=>"Materno");
+	$usu=${$narchivo}->mostrarTodo("nombre LIKE '%$nombre%' and paterno LIKE '%$paterno%' and materno LIKE '%$materno%' and nivel!=1 and $nivel");
+	$titulo=array("usuario"=>"Usuario","nombre"=>"Nombre","paterno"=>"Paterno","materno"=>"Materno","nivel"=>"Nivel");
+    $datos=array();
+    foreach($usu as $d){$i++;
+        $datos[$i]['codusuarios']=$d['codusuarios'];
+        $datos[$i]['nombre']=$d['nombre'];
+        $datos[$i]['paterno']=$d['paterno'];
+        $datos[$i]['materno']=$d['materno'];
+        $datos[$i]['usuario']=$d['usuario'];
+        switch($d['nivel']){
+            case '2':{$Nivel="Dirección";}break;   
+            case '3':{$Nivel="Profesor";}break;   
+            case '4':{$Nivel="Regente";}break;    
+        }
+        $datos[$i]['nivel']=$Nivel;
+    }
 	listadoTabla($titulo,$datos,1,"modificar.php","eliminar.php","ver.php");
 }
 ?>
