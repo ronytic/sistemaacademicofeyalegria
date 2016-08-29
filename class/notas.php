07a0e0743c019cf77f28809a46240c3f611f0e50
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-08-28 20:34:25 -0400

    Class Nota

--- class/notas.php
+++ class/notas.php
@@ -2,5 +2,17 @@
 include_once("bd.php");
 class notas extends bd{
 	var $tabla="notas";
+    function mejores($codcurso,$bimestre){
+        switch($bimestre){
+            case "1" :{$dato="bimestre".$bimestre;}break;
+            case "2" :{$dato="bimestre".$bimestre;}break;
+            case "3" :{$dato="bimestre".$bimestre;}break;
+            case "4" :{$dato="bimestre".$bimestre;}break;
+            case "Todos" :{$dato="notafinal";}break;    
+        }
+        //echo $dato;
+        $this->campos=array("avg($dato)as nota,codalumno");
+        return $this->getRecords("activo=1 and codalumno IN(SELECT codalumno FROM alumno WHERE codcurso=$codcurso) GROUP BY codalumno ORDER BY avg($dato) DESC");
+    }
 }
 ?>
